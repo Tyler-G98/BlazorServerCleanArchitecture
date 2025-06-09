17c2d0f744b8a75a077a9d229b42e7807461c9a0
Tyler G <tgartner062698@gmail.com>
2025-06-08 23:31:22 -0500

    Added 2 additional interfaces UnitOfWork: saves changes made by multiple repos Generic: Most functionality for CRUD methods

---
+++ BlazorServerCleanArchitecture.Application/Interfaces/Repositories/IGenericRepository.cs
@@ -0,0 +1,9 @@
+﻿public interface IGenericRepository<T> where T : class, IEntity
+{
+    IQueryable<T> Entities { get; }
+    Task<T> GetByIdAsync(int id);
+    Task<List<T>> GetAllAsync();
+    Task<T> AddAsync(T entity);
+    Task<T> UpdateAsync(T entity);
+    Task<T> DeleteAsync(T entity);
+}
---
+++ BlazorServerCleanArchitecture.Application/Interfaces/Repositories/IUnitOfWork.cs
@@ -0,0 +1,7 @@
+﻿public interface IUnitOfWork : IDisposable
+{
+    IGenericRepository<T> Repository<T>() where T : BaseAuditableEntity;
+    Task<int> Save(CancellationToken cancellationToken);
+    Task<int> SaveAndRemoveCache(CancellationToken cancellationToken, params string[] cacheKeys);
+    Task Rollback();
+}
